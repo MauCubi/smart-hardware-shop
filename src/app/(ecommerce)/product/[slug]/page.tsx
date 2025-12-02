417060d2558a23d1ab71cb701bdb55ffa57b258e
Mauro <dantuchiii@gmail.com>
2025-12-02 18:29:58 -0300

    Completed product swiper, added details and buttons to show product details

--- src/app/(ecommerce)/product/[slug]/page.tsx
+++ src/app/(ecommerce)/product/[slug]/page.tsx
@@ -1,6 +1,9 @@
 import SlideShow from '@/components/product/SlideShow';
 import { Product, products } from '@/data/products';
 import Link from 'next/link';
+import { FaMinusCircle, FaPlusCircle } from 'react-icons/fa';
+import { FaArrowDown, FaCartShopping, FaCheck, FaCross, FaMapLocation, FaShield, FaTruck, FaX } from 'react-icons/fa6';
+import { RiMoneyDollarCircleLine } from 'react-icons/ri';
 
 interface Props {
   params: Promise<{ slug: string }>
@@ -30,6 +33,78 @@ export default async function ProductDetailPage( { params }: Props) {
           </div>
 
           <h1 className='titles text-4xl'>{ prod?.name }</h1>
+
+
+          {
+            prod?.stock === 0 
+            ?
+            <div className='flex flex-row items-center mt-3 bg-red-600 rounded-md px-2 w-fit gap-1'>
+              <FaX color='#F1F1F1' />
+              <p className='titles'>Producto sin Stock</p>
+            </div>
+            : prod?.stock as number > 5
+              ?
+              <div className='flex flex-row items-center mt-3 bg-green-600 rounded-md px-2 w-fit gap-1'>
+                <FaCheck color='#F1F1F1' />
+                <p className='titles'>Producto en Stock</p>
+              </div>
+              :
+              <div className='flex flex-row items-center mt-3 bg-orange-600 rounded-md px-2 w-fit gap-1'>
+                <FaArrowDown color='#F1F1F1' />
+                <p className='titles'>Bajo en Stock</p>
+              </div>
+          }
+
+          <div className='flex my-8'>
+            <p className='titles text-5xl'>${ prod?.price.toFixed(2) }</p>
+          </div>
+
+          <div className='flex flex-col'>
+            <div className='flex flex-row border-b border-b-[#637a9181] titles items-center gap-2 p-3'>
+              <FaTruck size={30} color='#637a91'/> <p>Delivery to all the country</p>
+            </div>
+            <div className='flex flex-row border-b border-b-[#637a9181] titles items-center gap-2 p-3'>
+              <FaMapLocation size={30} color='#637a91'/> <p>Free Pickup at our store</p>
+            </div>
+            <div className='flex flex-row border-b border-b-[#637a9181] titles items-center gap-2 p-3'>
+              <FaShield size={30} color='#637a91'/> <p>150 days warranty</p>
+            </div>
+          </div>
+
+          <div className='flex flex-row justify-between'>
+            <div className='flex flex-row items-center titles p-3 mt-4'>
+              <p className='font-bold mr-6'>Quantity:</p>
+
+              <button className='cursor-pointer'>
+                <FaMinusCircle size={20} />
+              </button>
+
+              <div className='w-20 flex justify-center'>
+                <p> 10 Units </p>
+              </div>
+
+
+              <button className='cursor-pointer'>
+                <FaPlusCircle size={20} />
+              </button>
+            </div>
+
+            <div className='flex flex-row items-center mt-4 gap-3'>
+
+              <button className='cursor-pointer flex flex-row items-center px-4 py-2 gap-2 bg-[#0A84FF] rounded-lg text-[#F1F1F1] hover:bg-[#0a84ffad]'>
+                <RiMoneyDollarCircleLine size={30}/> <p className='font-bold'> Buy Now</p>
+              </button>
+
+              <button className='cursor-pointer flex flex-row items-center px-4 py-2 gap-2 border border-[#0A84FF] rounded-lg text-[#0A84FF] hover:border-[#0a84ffad] hover:text-[#0a84ffad]'>
+                <FaCartShopping size={30}/> <p className='font-bold'> Add to Cart</p>
+              </button>
+
+            </div>
+
+          </div>
+
+
+
         </div>
 
       </div>
--- src/components/product/SlideShow.tsx
+++ src/components/product/SlideShow.tsx
@@ -1,53 +1,143 @@
-'use client'
+'use client';
 
 import { Swiper, SwiperSlide } from 'swiper/react';
-import { Autoplay, FreeMode, Thumbs } from 'swiper/modules';
+import { Swiper as SwiperObject } from 'swiper';
+
+import { Autoplay, FreeMode, Thumbs, Navigation } from 'swiper/modules';
 
 // Import Swiper styles
 import 'swiper/css';
 import 'swiper/css/free-mode';
 import 'swiper/css/navigation';
 import 'swiper/css/thumbs';
+
+import './slideshow.css';
 import { useState } from 'react';
 import Image from 'next/image';
 
 const SlideShow = () => {
-
-  const [thumbsSwiper, setThumbsSwiper] = useState(null);
+  // const [thumbsSwiper, setThumbsSwiper] = useState(null);
+  const [thumbsSwiper, setThumbsSwiper] = useState<SwiperObject>();
 
   return (
-    <div className='w-[450]'>
+    <div className='block w-[500] h-[500] select-none'>
       <Swiper
-        style={{
-          '--swiper-navigation-color': '#fff',
-          '--swiper-pagination-color': '#fff',
-        } as React.CSSProperties
-      } 
+        style={
+          {
+            '--swiper-navigation-color': '#fff',
+            '--swiper-pagination-color': '#fff',
+          } as React.CSSProperties
+        }
         loop={true}
         spaceBetween={10}
-        navigation={true}
         thumbs={{ swiper: thumbsSwiper }}
         modules={[FreeMode, Thumbs, Autoplay]}
-        className="mySwiper2"
+        className='mySwiper2'
       >
         <SwiperSlide>
-          <Image src="/img/1024.png" alt='alt1' width={450} height={400} className='rounded-lg object-fill' />
+          <Image
+            src='/img/1024.png'
+            alt='alt1'
+            width={450}
+            height={400}
+            className='rounded-lg object-fill'
+          />
         </SwiperSlide>
         <SwiperSlide>
-          <Image src="/img/monitor1.png" alt='alt2' width={450} height={400} className='rounded-lg object-fill'/>
+          <Image
+            src='/img/10242.png'
+            alt='alt2'
+            width={450}
+            height={400}
+            className='rounded-lg object-fill'
+          />
         </SwiperSlide>
         <SwiperSlide>
-          <Image src="/img/carousel1.jpg" alt='alt3' width={450} height={400} className='rounded-lg object-fill'/>
+          <Image
+            src='/img/10243.png'
+            alt='alt3'
+            width={450}
+            height={400}
+            className='rounded-lg object-fill'
+          />
         </SwiperSlide>
         <SwiperSlide>
-          <Image src="/img/carousel2.jpg" alt='alt4' width={450} height={400} className='rounded-lg object-fill'/>
+          <Image
+            src='/img/10244.png'
+            alt='alt4'
+            width={450}
+            height={400}
+            className='rounded-lg object-fill'
+          />
         </SwiperSlide>
         <SwiperSlide>
-          <Image src="/img/carousel3.jpg" alt='alt5' width={450} height={400} className='rounded-lg object-fill'/>
+          <Image
+            src='/img/10245.png'
+            alt='alt5'
+            width={450}
+            height={400}
+            className='rounded-lg object-fill'
+          />
+        </SwiperSlide>
+      </Swiper>
+      <Swiper
+        onSwiper={setThumbsSwiper}
+        spaceBetween={10}
+        slidesPerView={4}
+        freeMode={true}
+        navigation={true}
+        watchSlidesProgress={true}
+        modules={[FreeMode, Thumbs, Navigation]}
+        className='mySwiper'
+      >
+        <SwiperSlide>
+          <Image
+            src='/img/1024.png'
+            alt='alt1'
+            width={100}
+            height={100}
+            className='rounded-lg object-fill'
+          />
+        </SwiperSlide>
+        <SwiperSlide>
+          <Image
+            src='/img/10242.png'
+            alt='alt2'
+            width={100}
+            height={100}
+            className='rounded-lg object-fill'
+          />
+        </SwiperSlide>
+        <SwiperSlide>
+          <Image
+            src='/img/10243.png'
+            alt='alt3'
+            width={100}
+            height={100}
+            className='rounded-lg object-fill'
+          />
+        </SwiperSlide>
+        <SwiperSlide>
+          <Image
+            src='/img/10244.png'
+            alt='alt4'
+            width={100}
+            height={100}
+            className='rounded-lg object-fill'
+          />
+        </SwiperSlide>
+        <SwiperSlide>
+          <Image
+            src='/img/10245.png'
+            alt='alt5'
+            width={100}
+            height={100}
+            className='rounded-lg object-fill'
+          />
         </SwiperSlide>
       </Swiper>
     </div>
-  )
-}
+  );
+};
 
-export default SlideShow
+export default SlideShow;
