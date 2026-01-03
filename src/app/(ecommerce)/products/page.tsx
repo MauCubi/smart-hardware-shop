aa3b8021150587cd7899cb1f791b770d33b76a44
Mauro <dantuchiii@gmail.com>
2026-01-03 16:09:48 -0300

    Added links to the subnav cards to filter page, also added ui for filter products

--- src/app/(ecommerce)/products/page.tsx
+++ src/app/(ecommerce)/products/page.tsx
@@ -1,9 +1,84 @@
-import React from 'react'
+import { IoFilterSharp } from 'react-icons/io5';
 
-const ProductsPage = () => {
-  return (
-    <div>ProductsPage</div>
-  )
+type SearchParams = Record<string, string | string[] | undefined>;
+
+interface Props {
+  searchParams: Promise<SearchParams>;
 }
 
-export default ProductsPage
+export default async function SearchPage({ searchParams }: Props) {
+  const filters = await searchParams;
+
+  
+
+  return (
+    <div className='h-200 pt-20'>
+      
+      <h1 className='text-3xl titles px-8 mb-2'>{ filters['subcategory'] ? JSON.stringify(filters['subcategory'], null, 2).replace(/"/g, "") : JSON.stringify(filters['category'], null, 2).replace(/"/g, "") }</h1>
+
+      <div className='flex flex-col justify-center px-8'>
+        <label htmlFor="sort" className="block mb-2.5 text-sm font-bold text-heading titles">Order By</label>
+        <select
+          id='sort'
+          className='block w-full px-3 py-2.5  border border-default-medium border-[#0A84FF] rounded titles text-heading text-sm rounded-base shadow-xs'          
+        >          
+          <option className='text-gray-900 rounded' value='US'>All</option>
+          <option className='text-gray-900 rounded' value='CA'>Highest Price</option>
+          <option className='text-gray-900 rounded' value='FR'>Lowest Price</option>          
+        </select>
+      </div>
+
+      <div className='flex justify-center mt-4 items-center w-full'>
+        <button className='flex gap-1 border border-white rounded-lg py-2 px-3'>
+          <IoFilterSharp className='titles text-2xl'/>
+          <h1 className='titles text-lg'>Filters</h1>
+        </button>
+      </div>
+
+      <div className='w-full h-fit pb-8 bg-[#181818] absolute'>
+        <h1 className='titles text-3xl p-8'>Filters</h1>
+
+        <div className='flex flex-col px-8 mb-4'>
+          <h1 className='titles text-2xl font-bold text-[#7eb7ef]'>Filter Title One</h1>
+          <div className='flex justify-between'>
+            <p className='titles'>Filter one</p>
+            <span className='titles'>(2)</span>
+          </div>
+          <div className='flex justify-between'>
+            <p className='titles'>Filter two</p>
+            <span className='titles'>(4)</span>
+          </div>
+          <div className='flex justify-between'>
+            <p className='titles'>Filter one</p>
+            <span className='titles'>(1)</span>
+          </div>
+        </div>
+
+        <div className='flex flex-col px-8'>
+          <h1 className='titles text-2xl font-bold text-[#7eb7ef]'>Filter Title Two</h1>
+          <div className='flex justify-between'>
+            <p className='titles'>Filter one</p>
+            <span className='titles'>(2)</span>
+          </div>
+          <div className='flex justify-between'>
+            <p className='titles'>Filter two</p>
+            <span className='titles'>(4)</span>
+          </div>
+          <div className='flex justify-between'>
+            <p className='titles'>Filter one</p>
+            <span className='titles'>(1)</span>
+          </div>
+        </div>
+
+
+      </div>
+
+      
+
+      <div className='w-full h-20 '>
+
+      </div>
+
+    </div>
+  );
+}
--- src/components/ui/navbar/CategoryCard.tsx
+++ src/components/ui/navbar/CategoryCard.tsx
@@ -1,3 +1,6 @@
+import { useAppDispatch, useAppSelector } from '@/store/hooks';
+import { onToggleSideMenu } from '@/store/ui/uiSlice';
+import Link from 'next/link';
 import { useState } from 'react'
 import { IconType } from 'react-icons'
 
@@ -11,17 +14,28 @@ interface Props {
 
 export const CategoryCard = ({ title, subCategories, Icon }: Props) => {
 
-  const [showMore, setShowMore] = useState<boolean>(false)  
+  const [showMore, setShowMore] = useState<boolean>(false)
+
+  const dispatch = useAppDispatch()
+
+    const handleSideMenu = () => {
+      dispatch(onToggleSideMenu(false))
+    }
 
 
   return (
     <div className='flex flex-col h-60 xl:min-h-60 min-w-40 xl:min-w-50 px-2 md:px-5 lg:px-2'>
       <Icon className='text-[40px] xl:text-[60px]' color='#0A84FF'/>
-      <h1 className='font-navbar font-bold text-md xl:text-lg text-[#0A84FF] my-3'>{ title }</h1>
+      
+      <Link href={`products/?category=${title}`} onClick={ handleSideMenu }>
+        <h1 className='font-navbar font-bold text-md xl:text-lg text-[#0A84FF] my-3'>{ title }</h1>
+      </Link>
 
       {
         subCategories.slice(0,4).map( subCategory => (
-          <p key={subCategory} className='titles xl:text-md text-sm tracking-normal'>{subCategory}</p>
+          <Link key={subCategory} href={`products/?subcategory=${subCategory}`} onClick={ handleSideMenu }>
+            <p className='titles xl:text-md text-sm tracking-normal'>{subCategory}</p>
+          </Link>
         ))        
       }
 
