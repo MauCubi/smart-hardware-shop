e2ccefc679652d0a55e9ef33ee044272d054ab9c
Mauro <dantuchiii@gmail.com>
2026-01-23 15:49:10 -0300

    fixed some url path issues

--- src/components/ui/navbar/CategoryCard.tsx
+++ src/components/ui/navbar/CategoryCard.tsx
@@ -1,4 +1,4 @@
-import { useAppDispatch, useAppSelector } from '@/store/hooks';
+import { useAppDispatch } from '@/store/hooks';
 import { onToggleProductsMenu, onToggleSideMenu } from '@/store/ui/uiSlice';
 import Link from 'next/link';
 import { useState } from 'react'
@@ -28,13 +28,13 @@ export const CategoryCard = ({ title, subCategories, Icon }: Props) => {
     <div className='flex flex-col h-60 xl:min-h-60 min-w-40 xl:min-w-50 px-2 md:px-5 lg:px-2'>
       <Icon className='text-[40px] xl:text-[60px]' color='#0A84FF'/>
       
-      <Link href={`products/?category=${title}`} onClick={ handleSideMenu }>
+      <Link href={`../products/?category=${title}`} onClick={ handleSideMenu }>
         <h1 className='font-navbar font-bold text-md xl:text-lg text-[#0A84FF] my-3'>{ title }</h1>
       </Link>
 
       {
         subCategories.slice(0,4).map( subCategory => (
-          <Link key={subCategory} href={`products/?subcategory=${subCategory}`} onClick={ handleSideMenu }>
+          <Link key={subCategory} href={`../products/?subcategory=${subCategory}`} onClick={ handleSideMenu }>
             <p className='titles xl:text-md text-sm tracking-normal'>{subCategory}</p>
           </Link>
         ))        
