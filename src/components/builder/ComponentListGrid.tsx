267d73e7d0b2bb0184a7b55e6aa119a18259c405
Mauro <dantuchiii@gmail.com>
2026-03-19 14:11:14 -0300

    fixed filtering in builder

--- src/components/builder/ComponentListGrid.tsx
+++ src/components/builder/ComponentListGrid.tsx
@@ -1,20 +1,37 @@
 import { getPaginatedProducts } from '@/actions/product/get-paginated-products'
 import { ComponentListCard } from './ComponentListCard'
+import { useEffect, useState } from 'react'
+import { useAppDispatch, useAppSelector } from '@/store/hooks'
+import { Product } from '@/types/product'
 
 interface Props {
   component: string | null
 }
 
-export const ComponentListGrid = async ({ component }: Props) => {
+export const ComponentListGrid =  ({ component }: Props) => {
 
-  const page = 1
+  const [filteredProducts, setFilteredProducts] = useState<Product[]>([])
 
-  const { products, maxPages }  = await getPaginatedProducts({page})
+ 
+  
+  useEffect(() => {
+    
+    const fetchProducts = async () => {
+      const page = 1
+      const { products, maxPages }  = await getPaginatedProducts({page})     
+      setFilteredProducts(products) 
+    }
+    console.log('ehhh')
+  
+    fetchProducts()
+  }, [component])
+  
+  
 
 
   return (
     <div className='grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 grid-rows-2-2 mx-auto gap-10 my-6 content-center px-8'>
-      {products.map((product) => (
+      {filteredProducts.map((product) => (
         <div className='flex justify-center rounded-md' key={product.id}>
           <ComponentListCard product={product} />
         </div>
